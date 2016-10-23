d04f0dab0174968d4f3ddd5e159b372ef56f9913
Cameron Probert <c.probert@hotmail.co.nz>
2016-10-23 20:45:29 +0900

    Added the very basic mod
    Added .modinfo and rules.sql files

---
+++ Less Annoying AI/Rules.sql
@@ -0,0 +1,15 @@
+/*
+	Less annoying AI
+	by Cameron Probert (2016)
+	original idea by reddit user /u/malvekiar
+
+*/
+ 
+
+-----------------------------------------------
+-- AI talk rates
+-----------------------------------------------
+
+UPDATE GlobalParamaters SET AI_TURNS_BETWEEN_FRIENDSHIP_OFFERS = 10;
+UPDATE GlobalParamaters SET AI_TURNS_BETWEEN_PEACE_OFFERS = 5;
+UPDATE GlobalParamaters SET AI_TURNS_BETWEEN_FRIENDSHIP_OFFERS = 12;
